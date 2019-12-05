31b818679e271c3a0f6f0eda108d4e1c9b920039
katerynak <konotopska.k@gmail.com>
2019-12-05 22:32:05 +0100

    path fixing

--- __hpc/_smart_initialization_community_detection_in_short/_amazon_population_size_WC.sh
+++ __hpc/_smart_initialization_community_detection_in_short/_amazon_population_size_WC.sh
@@ -15,5 +15,5 @@ module load python-3.7.2
 # pip3.7 install community --user
 # pip3.7 install python-louvain --user
 cd Influence-Maximization/src/
-python3 experiments.py --exp_dir=../smart_initialization/community_detection/in/amazon/population_size/WC --hpc=True
+python3 experiments.py --exp_dir=../experiments/smart_initialization/community_detection/in/amazon/population_size/WC --hpc=True
 	
