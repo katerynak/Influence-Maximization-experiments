c7fba1a0ea8cfc2ed223aa83993ea7c4f94c187f
katerynak <konotopska.k@gmail.com>
2019-12-05 21:22:37 +0100

    hpc installing libraries user parameter

--- __hpc/split_jobs.py
+++ __hpc/split_jobs.py
@@ -4,11 +4,12 @@ splits jobs of the desired subdirectory level
 import os
 from src.utils import traverse_level
 
-in_dir = "../experiments/spread_functions_correlation/in"
+in_dir = "../smart_initialization/community_detection/in"
+# in_dir = "../experiments/spread_functions_correlation/in"
 # in_dir = "../experiments/smart_initialization_comparison/in"
 # in_dir = "../experiments/spread_functions_comparisons_ea/in"
 
-split_level = 2
+split_level = 4
 
 sub_directories = []
 level_dirs = traverse_level(in_dir, split_level)
@@ -17,9 +18,9 @@ for lev_dir, _, _ in level_dirs:
 
 
 n_cpus = 4
-mem = 32
-walltime = 6
-queue = "short"
+mem = 64
+walltime = 100
+queue = "common"
 
 
 directory = in_dir.replace("..", "").replace("/", "_") + "_" + queue
@@ -28,7 +29,7 @@ if not os.path.exists(directory):
 
 for sub_dir in sub_directories:
 	if "amazon" in sub_dir:
-		mem=64
+		mem = 256
 	shell_text = """#!/bin/bash
 
 #PBS -l select=1:ncpus={}:mem={}gb
@@ -39,9 +40,12 @@ for sub_dir in sub_directories:
 # set the queue
 #PBS -q {}_cpuQ
 module load python-3.7.2
-pip3 install inspyred
-pip3 install networkx
-pip3 install numpy
+pip3.7 install inspyred --user
+pip3.7 install networkx --user
+pip3.7 install numpy --user
+pip3.7 install node2vec --user
+pip3.7 install community --user
+pip3.7 install python-louvain --user
 cd Influence-Maximization/src/
 python3 experiments.py --exp_dir={} --hpc=True
 	""".format(n_cpus, mem, walltime, queue, sub_dir)
