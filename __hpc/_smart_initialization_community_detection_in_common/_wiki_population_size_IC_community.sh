c34f07a07dc93b17c24879b809c247602c6c683d
katerynak <konotopska.k@gmail.com>
2019-12-05 22:34:59 +0100

    path fixing

--- __hpc/_smart_initialization_community_detection_in_common/_wiki_population_size_IC_community.sh
+++ __hpc/_smart_initialization_community_detection_in_common/_wiki_population_size_IC_community.sh
@@ -8,12 +8,12 @@
 # set the queue
 #PBS -q common_cpuQ
 module load python-3.7.2
-pip3.7 install inspyred --user
-pip3.7 install networkx --user
-pip3.7 install numpy --user
-pip3.7 install node2vec --user
-pip3.7 install community --user
-pip3.7 install python-louvain --user
+# pip3.7 install inspyred --user
+# pip3.7 install networkx --user
+# pip3.7 install numpy --user
+# pip3.7 install node2vec --user
+# pip3.7 install community --user
+# pip3.7 install python-louvain --user
 cd Influence-Maximization/src/
-python3 experiments.py --exp_dir=../smart_initialization/community_detection/in/wiki/population_size/IC/community --hpc=True
+python3 experiments.py --exp_dir=../experiments/smart_initialization/community_detection/in/wiki/population_size/IC/community --hpc=True
 	
