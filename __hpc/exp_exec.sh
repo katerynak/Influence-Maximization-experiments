f107637eaafe0b554c2a3d31e7bb3f57932a9b5e
katerynak <konotopska.k@gmail.com>
2019-12-05 21:21:52 +0100

    hpc installing libraries user parameter

--- __hpc/exp_exec.sh
+++ __hpc/exp_exec.sh
@@ -11,12 +11,12 @@
 # set the queue
 #PBS -q common_cpuQ
 module load python-3.7.2
-pip3 install inspyred
-pip3 install networkx
-pip3 install numpy
-pip3 install node2vec
-pip3 install community
-pip3 install python-louvain
+pip3.7 install inspyred --user
+pip3.7 install networkx --user
+pip3.7 install numpy --user
+pip3.7 install node2vec --user
+pip3.7 install community --user
+pip3.7 install python-louvain --user
 cd Influence-Maximization/src/
 #python3 experiments.py --exp_dir=../experiments/test --hpc=True
 python3 embeddings.py
