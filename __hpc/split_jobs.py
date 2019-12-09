d1377680605a6330b16bce15a2798247456af8a4
katerynak <konotopska.k@gmail.com>
2019-12-09 12:37:05 +0100

    spectral clustering experimental setup

--- __hpc/split_jobs.py
+++ __hpc/split_jobs.py
@@ -9,7 +9,19 @@ in_dir = "../smart_initialization/community_detection/in"
 # in_dir = "../experiments/smart_initialization_comparison/in"
 # in_dir = "../experiments/spread_functions_comparisons_ea/in"
 
+n_cpus = 4
+def_mem = 64
+queue = "short"
+# queue = "common"
+
 split_level = 4
+
+if queue=="short":
+	walltime = 6
+	split_level = 3
+elif queue=="common":
+	split_level = 4
+	walltime = 100
 
 sub_directories = []
 level_dirs = traverse_level(in_dir, split_level)
@@ -17,10 +29,7 @@ for lev_dir, _, _ in level_dirs:
 	sub_directories.append(lev_dir)
 
 
-n_cpus = 4
-def_mem = 64
-walltime = 100
-queue = "common"
+
 
 
 directory = in_dir.replace("..", "").replace("/", "_") + "_" + queue
@@ -29,7 +38,7 @@ if not os.path.exists(directory):
 
 for sub_dir in sub_directories:
 	if "amazon" in sub_dir:
-		mem = 256
+		mem = 128
 	else:
 		mem = def_mem
 	shell_text = """#!/bin/bash
@@ -42,15 +51,15 @@ for sub_dir in sub_directories:
 # set the queue
 #PBS -q {}_cpuQ
 module load python-3.7.2
-pip3.7 install inspyred --user
-pip3.7 install networkx --user
-pip3.7 install numpy --user
-pip3.7 install node2vec --user
-pip3.7 install community --user
-pip3.7 install python-louvain --user
+# pip3.7 install inspyred --user
+# pip3.7 install networkx --user
+# pip3.7 install numpy --user
+# pip3.7 install node2vec --user
+# pip3.7 install community --user
+# pip3.7 install python-louvain --user
 cd Influence-Maximization/src/
 python3 experiments.py --exp_dir={} --hpc=True
-	""".format(n_cpus, mem, walltime, queue, sub_dir)
+	""".format(n_cpus, mem, walltime, queue, sub_dir.replace("..", "../experiments"))
 	filename = sub_dir.replace(in_dir, "").replace("/", "_") + ".sh"
 	with open(directory + "/" + filename, "w") as f:
 		f.write(shell_text)
