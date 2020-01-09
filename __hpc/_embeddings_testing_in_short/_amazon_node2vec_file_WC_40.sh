12ed66474b682fd4059df263c0960f0596d7d073
katerynak <konotopska.k@gmail.com>
2020-01-09 16:03:09 +0100

    bigger splitting

--- __hpc/_embeddings_testing_in_short/_amazon_node2vec_file_WC_40.sh
+++ __hpc/_embeddings_testing_in_short/_amazon_node2vec_file_WC_40.sh
@@ -15,5 +15,5 @@ module load python-3.7.2
 # pip3.7 install community --user
 # pip3.7 install python-louvain --user
 cd Influence-Maximization/src/
-python3 experiments.py --exp_dir=../experiments/embeddings_testing/in/amazon/node2vec_file/WC --hpc=True
+python3 experiments.py --exp_dir=../experiments/embeddings_testing/in/amazon/node2vec_file/WC/40 --hpc=True
 	
