fde1c082ba389372651e7458f178d5a3322462f9
katerynak <konotopska.k@gmail.com>
2020-01-11 18:12:56 +0100

    wiki embeddings testing in

---
+++ __hpc/_embeddings_testing_in_wiki_short/_node2vec_file_WC_50__experiments_node2vec_embeddings_training_out_wiki_iter_dimensions_32_1_seed_3_exp_in_repetition_0_embeddings_iter_1__emb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=4:mem=32gb
+
+# set max execution time
+#PBS -l walltime=6:00:0
+
+# set the queue
+#PBS -q short_cpuQ
+module load python-3.7.2
+# pip3.7 install inspyred --user
+# pip3.7 install networkx --user
+# pip3.7 install numpy --user
+# pip3.7 install node2vec --user
+# pip3.7 install community --user
+# pip3.7 install python-louvain --user
+cd Influence-Maximization/src/
+python3 experiments.py --exp_dir=../experiments/embeddings_testing/in/wiki/node2vec_file/WC/50/_experiments_node2vec_embeddings_training_out_wiki_iter_dimensions_32_1_seed_3_exp_in_repetition_0_embeddings_iter_1__emb --hpc=True
+	
