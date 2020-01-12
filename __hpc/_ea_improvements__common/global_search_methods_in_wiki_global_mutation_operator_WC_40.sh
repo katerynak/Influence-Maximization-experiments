48362f4befb404dc8a07a567d0ad6ba28f7fbdcd
katerynak <konotopska.k@gmail.com>
2020-01-12 18:10:54 +0100

    hpc setup

---
+++ __hpc/_ea_improvements__common/global_search_methods_in_wiki_global_mutation_operator_WC_40.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=4:mem=32gb
+
+# set max execution time
+#PBS -l walltime=24:00:0
+
+# set the queue
+#PBS -q common_cpuQ
+module load python-3.7.2
+# pip3.7 install inspyred --user
+# pip3.7 install networkx --user
+# pip3.7 install numpy --user
+# pip3.7 install node2vec --user
+# pip3.7 install community --user
+# pip3.7 install python-louvain --user
+cd Influence-Maximization/src/
+python3 experiments.py --exp_dir=../experiments/ea_improvements/global_search_methods/in/wiki/global_mutation_operator/WC/40 --hpc=True
+	
