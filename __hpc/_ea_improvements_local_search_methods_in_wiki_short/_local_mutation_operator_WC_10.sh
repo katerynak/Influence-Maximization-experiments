c7093c9135ec4a7b9e7263c3ac8ccedfef92a20c
katerynak <konotopska.k@gmail.com>
2020-01-13 13:01:24 +0100

    wiki split

---
+++ __hpc/_ea_improvements_local_search_methods_in_wiki_short/_local_mutation_operator_WC_10.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=1:mem=24gb
+
+# set max execution time
+#PBS -l walltime=6:00:0
+
+# set the queue
+#PBS -q short_cpuQ
+module load python-3.7.2
+# pip3.7 install inspyred --user
+# pip3.7 install networkx --user
+# pip3.7 install numpy --user
+# pip3.7 install node2vec --user
+# pip3.7 install community --user
+# pip3.7 install python-louvain --user
+cd Influence-Maximization/src/
+python3 experiments.py --exp_dir=../experiments/ea_improvements/local_search_methods/in/wiki/local_mutation_operator/WC/10 --hpc=True
+	
