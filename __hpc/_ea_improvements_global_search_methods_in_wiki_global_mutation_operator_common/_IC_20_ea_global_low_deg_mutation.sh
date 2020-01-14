c8ae12956f68f2186061252b36f95164c861c12a
katerynak <konotopska.k@gmail.com>
2020-01-14 11:15:08 +0100

    mutliprocessing

---
+++ __hpc/_ea_improvements_global_search_methods_in_wiki_global_mutation_operator_common/_IC_20_ea_global_low_deg_mutation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=8:mem=128gb
+
+# set max execution time
+#PBS -l walltime=40:00:0
+
+# set the queue
+#PBS -q common_cpuQ
+module load python-3.7.2
+# pip3.7 install inspyred --user
+# pip3.7 install networkx --user
+# pip3.7 install numpy --user
+# pip3.7 install node2vec --user
+# pip3.7 install community --user
+# pip3.7 install python-louvain --user
+cd Influence-Maximization/src/
+python3 experiments.py --exp_dir=../experiments/ea_improvements/global_search_methods/in/wiki/global_mutation_operator/IC/20/ea_global_low_deg_mutation --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements/global_search_methods/in/wiki/global_mutation_operator/IC/30/ea_global_subpopulation_mutation --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements/global_search_methods/in/wiki/global_mutation_operator/IC/30/ea_global_random_mutation --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements/global_search_methods/in/wiki/global_mutation_operator/IC/30/ea_global_low_deg_mutation --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements/global_search_methods/in/wiki/global_mutation_operator/IC/50/ea_global_subpopulation_mutation --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements/global_search_methods/in/wiki/global_mutation_operator/IC/50/ea_global_random_mutation --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements/global_search_methods/in/wiki/global_mutation_operator/IC/50/ea_global_low_deg_mutation --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements/global_search_methods/in/wiki/global_mutation_operator/WC/10/ea_global_subpopulation_mutation --hpc=True & 
+wait
