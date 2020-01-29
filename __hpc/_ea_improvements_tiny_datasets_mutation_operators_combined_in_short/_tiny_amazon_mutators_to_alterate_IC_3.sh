3e4a0bc01683de5a9c746f69191e7ee459cfa689
katerynak <konotopska.k@gmail.com>
2020-01-29 21:13:27 +0100

    tiny datasets mutation combinations combined

---
+++ __hpc/_ea_improvements_tiny_datasets_mutation_operators_combined_in_short/_tiny_amazon_mutators_to_alterate_IC_3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=8:mem=32gb
+
+# set max execution time
+#PBS -l walltime=6:00:0
+
+# set the queue
+#PBS -q short_cpuQ
+module load python-3.7.2
+# pip3.7 install inspyred --user
+# pip3.7 install networkx --user
+# pip3.7 install numpy --user
+# pip3.7 install node2vec --user
+# pip3.7 install community --user
+# pip3.7 install python-louvain --user
+cd Influence-Maximization/src/
+python3 experiments.py --exp_dir=../experiments/ea_improvements_tiny_datasets/mutation_operators_combined/in/tiny_amazon/mutators_to_alterate/IC/3 --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements_tiny_datasets/mutation_operators_combined/in/tiny_amazon/mutators_to_alterate/IC/2 --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements_tiny_datasets/mutation_operators_combined/in/tiny_amazon/mutators_to_alterate/WC/3 --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements_tiny_datasets/mutation_operators_combined/in/tiny_amazon/mutators_to_alterate/WC/2 --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements_tiny_datasets/mutation_operators_combined/in/tiny_wiki_community/mutators_to_alterate/IC/3 --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements_tiny_datasets/mutation_operators_combined/in/tiny_wiki_community/mutators_to_alterate/IC/2 --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements_tiny_datasets/mutation_operators_combined/in/tiny_wiki_community/mutators_to_alterate/WC/3 --hpc=True & python3 experiments.py --exp_dir=../experiments/ea_improvements_tiny_datasets/mutation_operators_combined/in/tiny_wiki_community/mutators_to_alterate/WC/2 --hpc=True & 
+wait
