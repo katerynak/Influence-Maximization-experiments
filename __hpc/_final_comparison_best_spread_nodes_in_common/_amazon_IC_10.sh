852ee705d32caa3ab965bddaaaf34ded66ddee6b
katerynak <konotopska.k@gmail.com>
2020-03-05 15:38:58 +0100

    best spread nods + multiple mutations final comparison setup

---
+++ __hpc/_final_comparison_best_spread_nodes_in_common/_amazon_IC_10.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=4:mem=32gb
+
+# set max execution time
+#PBS -l walltime=100:00:0
+
+# set the queue
+#PBS -q common_cpuQ
+module load python-3.7.2
+# pip3.7 install inspyred --user
+# pip3.7 install networkx --user
+# pip3.7 install numpy --user
+# pip3.7 install node2vec --user
+# pip3.7 install community --user
+# pip3.7 install python-louvain --user
+cd Influence-Maximization/src/
+python3 experiments.py --exp_dir=../experiments/final_comparison/best_spread_nodes/in/amazon/IC/10 --hpc=True & python3 experiments.py --exp_dir=../experiments/final_comparison/best_spread_nodes/in/amazon/IC/40 --hpc=True & python3 experiments.py --exp_dir=../experiments/final_comparison/best_spread_nodes/in/amazon/IC/20 --hpc=True & python3 experiments.py --exp_dir=../experiments/final_comparison/best_spread_nodes/in/amazon/IC/30 --hpc=True & 
+wait
