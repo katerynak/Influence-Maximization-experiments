15867941df6e8136941cd921983d2565ab82912e
katerynak <konotopska.k@gmail.com>
2020-03-05 16:00:09 +0100

    single mutations final comparison setup

---
+++ __hpc/_final_comparison_single_mutations_in_short/_amazon_IC_10.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=4:mem=32gb
+
+# set max execution time
+#PBS -l walltime=6:00:0
+
+# set the queue
+#PBS -q short_cpuQ
+module load python-3.7.2
+# pip3.7 install inspyred --user
+# pip3.7 install networkx --user
+# pip3.7 install numpy --user
+# pip3.7 install node2vec --user
+# pip3.7 install community --user
+# pip3.7 install python-louvain --user
+cd Influence-Maximization/src/
+python3 experiments.py --exp_dir=../experiments/final_comparison/single_mutations/in/amazon/IC/10 --hpc=True & python3 experiments.py --exp_dir=../experiments/final_comparison/single_mutations/in/amazon/IC/40 --hpc=True & python3 experiments.py --exp_dir=../experiments/final_comparison/single_mutations/in/amazon/IC/20 --hpc=True & python3 experiments.py --exp_dir=../experiments/final_comparison/single_mutations/in/amazon/IC/30 --hpc=True & 
+wait
